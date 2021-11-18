94555ffd4ae9eb1572906e546016d4b1aef5c85a
Hendi Santika <hendisantika@gmail.com>
2021-11-19 05:54:36 +0700

    #8 - Add UserStatus Enum

---
+++ src/main/java/com/hendisantika/entity/UserStatus.java
@@ -0,0 +1,14 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-registration-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/21
+ * Time: 05.54
+ */
+public enum UserStatus {
+    HOLD, ACTIVE, INACTIVE, SUSPENDED
+}
