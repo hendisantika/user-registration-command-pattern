1170c028554226331786bd7bae7c842fc39fad9e
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:08:41 +0700

    #21 - Add handleUserCreatedEvent in UserEventPublisherImpl

--- src/main/java/com/hendisantika/event/publisher/UserEventPublisherImpl.java
+++ src/main/java/com/hendisantika/event/publisher/UserEventPublisherImpl.java
@@ -1,10 +1,17 @@
 package com.hendisantika.event.publisher;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.event.UserSource;
+import com.hendisantika.event.model.UserCreatedEvent;
+import com.hendisantika.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +34,24 @@ public class UserEventPublisherImpl implements UserEventPublisher {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Override
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
+        User createdUser = userRepository.findById(userCreatedEvent.getId()).get();
+        System.out.println(createdUser.getVerification().toString());
+
+        UserEventSnapshot userEventSnapshot = new UserEventSnapshot(createdUser.getId(),
+                createdUser.getUsername(),
+                createdUser.getFirstName(),
+                createdUser.getLastName(),
+                createdUser.getEmail(),
+                createdUser.getStatus(),
+                createdUser.getVerification().getToken());
+
+        System.out.println(userEventSnapshot);
+
+        userSource.userChannel().send(MessageBuilder.withPayload(userEventSnapshot).build());
+    }
 }
