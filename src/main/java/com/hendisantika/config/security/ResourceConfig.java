9cdc429962c068cb40fa00b18a2d579449b9eea4
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:30:57 +0700

    #44 - Add configure in ResourceConfig

--- src/main/java/com/hendisantika/config/security/ResourceConfig.java
+++ src/main/java/com/hendisantika/config/security/ResourceConfig.java
@@ -3,8 +3,11 @@ package com.hendisantika.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
@@ -29,4 +32,24 @@ public class ResourceConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
     private TokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources
+                .resourceId(resourceId)
+                .tokenServices(tokenServices)
+                .tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatcher(new OAuthRequestedMatcher())
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/api/users").access("hasAnyRole('USER')")
+                .antMatchers("/api/**").authenticated();
+    }
 }
