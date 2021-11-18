5640d11affa65deb06ee2301b4dc0e367dac1c09
Hendi Santika <hendisantika@gmail.com>
2021-11-19 05:55:29 +0700

    #10 - Add Role Enum

---
+++ src/main/java/com/hendisantika/entity/Role.java
@@ -0,0 +1,18 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-registration-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/21
+ * Time: 05.55
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_REGISTER,
+    ROLE_TRUSTED_CLIENT,
+    ROLE_CLIENT
+}
