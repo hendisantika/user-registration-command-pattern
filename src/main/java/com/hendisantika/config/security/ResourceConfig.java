cb5b20f702834ea7033d835e0d9058cbf6d6b240
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:30:20 +0700

    #43 - Add ResourceConfig

---
+++ src/main/java/com/hendisantika/config/security/ResourceConfig.java
@@ -0,0 +1,32 @@
+package com.hendisantika.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-registration-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/21
+ * Time: 06.29
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Autowired
+    private DefaultTokenServices tokenServices;
+
+    @Autowired
+    private TokenStore tokenStore;
+}
