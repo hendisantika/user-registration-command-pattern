6bf51c4c83bae9ec3846f58033af08dcd75b50d3
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:26:33 +0700

    #38 - Add SwaggerConfig

---
+++ src/main/java/com/hendisantika/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-registration-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/21
+ * Time: 06.25
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.domain.user"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "User Management",
+                "Handle the user management activities",
+                "1.0.0",
+                "Terms of Service",
+                new Contact("Hendi santika", "hendisantika.com", "hendisantika@yahoo.co.id"),
+                "",
+                "",
+                Collections.emptyList());
+    }
+}
