f10c06283999df178b86a0b277885852efe27fdd
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:31:55 +0700

    #46 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.hendisantika.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-registration-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/21
+ * Time: 06.31
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public UserDetailsService userDetailsService;
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+}
