4bf3aa76dce11419ee210bba812d071c26872117
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:33:17 +0700

    #49 - Add OAuthRequestedMatcher in SecurityConfig

--- src/main/java/com/hendisantika/config/security/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by IntelliJ IDEA.
@@ -64,4 +68,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .password("{noop}password").authorities("ROLE_ADMIN");
     }
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    private static class OAuthRequestedMatcher implements RequestMatcher {
+        public boolean matches(HttpServletRequest request) {
+            // Determine if the resource called is "/api/**"
+            String path = request.getServletPath();
+            if (path.length() >= 5) {
+                path = path.substring(0, 5);
+                boolean isApi = path.equals("/api/");
+                return isApi;
+            } else return false;
+        }
+    }
 }
