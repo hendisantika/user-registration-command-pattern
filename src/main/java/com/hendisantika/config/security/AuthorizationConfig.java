6d05d1d36de14ac86878869d9b6623bf7325bd00
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:27:39 +0700

    #40 - Add userDetailsService in AuthorizationConfig

--- src/main/java/com/hendisantika/config/security/AuthorizationConfig.java
+++ src/main/java/com/hendisantika/config/security/AuthorizationConfig.java
@@ -1,6 +1,13 @@
 package com.hendisantika.config.security;
 
+import com.hendisantika.service.query.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
@@ -19,4 +26,19 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
     private final int accessTokenValiditySeconds = 10000;
     private final int refreshTokenValiditySeconds = 30000;
+
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
 }
