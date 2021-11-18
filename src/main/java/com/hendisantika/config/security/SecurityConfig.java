a71f2a57e762c098164f8fe498856e1d93be4eb8
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:32:49 +0700

    #48 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/config/security/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,5 +41,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .anonymous().disable()
+                .httpBasic().and()
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll().and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("{noop}password").roles("USER")
+//                .and()
+//                .withUser("admin")
+//                .password("{noop}password").authorities("ROLE_ADMIN");
+    }
 
 }
