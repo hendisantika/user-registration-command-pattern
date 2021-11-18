ae4a88163273fc09467001d656c62d9e2b57af6f
Hendi Santika <hendisantika@gmail.com>
2021-11-19 05:33:03 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/UserRegistrationCommandPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationCommandPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationCommandPatternApplication.class, args);
+    }
+
+}
