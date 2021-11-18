b9bf9742cde34f00eeccafd8e11eb69d7a213763
Hendi Santika <hendisantika@gmail.com>
2021-11-19 06:36:39 +0700

    #54 - Add createNewUser API in UserCommandController

--- src/main/java/com/hendisantika/controller/UserCommandController.java
+++ src/main/java/com/hendisantika/controller/UserCommandController.java
@@ -1,12 +1,21 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.domain.commands.UserCreateDTO;
 import com.hendisantika.event.UserSource;
 import com.hendisantika.service.command.UserCommandService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +37,12 @@ public class UserCommandController {
 
     @Autowired
     private UserSource userSource;
+
+    @PostMapping
+    @ApiOperation("Registers a new user")
+    @ResponseStatus(HttpStatus.CREATED)
+    private ResponseEntity<String> createNewUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
+        return new ResponseEntity<>("New User Created: " + userCommandService.createUser(userCreateDTO),
+                HttpStatus.CREATED);
+    }
 }
